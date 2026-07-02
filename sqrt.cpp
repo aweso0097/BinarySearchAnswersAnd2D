52fb51365b05befa3ec65c799d2c9300693087dd
aweso0097 <aweso1045@gmail.com>
2026-07-02 12:47:46 +0530

    Implement floor square root function in sqrt.cpp
    Using Binary Search to find the floor square root of a number n.

---
+++ sqrt.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int floorSqrt(int n)  {
+      int low = 1;
+      int high = n;
+      while(low <= high) {
+        int mid = low + (high-low)/2;
+        if(mid*mid < n) {
+            low = mid+1;
+        }
+        else {
+            high = mid;
+        }
+      }
+      return high;
+    }
+};
